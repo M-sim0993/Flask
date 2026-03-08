5f10a711e88e3b713efbfcabe895de0faf777280
M-sim0993 <m-sim0993@mail.ru>
2026-03-08 18:15:48 +0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,1 +1,45 @@
 from app import app
+
+# Определяем маршрут и привязываем его к функции
+@app.route('/')
+def hello():
+    return "Hello, Flask!"
+
+@app.route('/hello')
+def hello_word():
+    return "Hello, world!"
+
+@app.route('/info')
+def info():
+    return "This is an informational page."
+
+@app.route('/calc/<s1>/<s2>')
+def calc(s1, s2):
+    if is_number(s1) & is_number(s2):
+        return f"The sum of {s1} and {s2} is {int(s1) + int(s2)}."
+    else:
+        return "Uncorrect data."
+
+@app.route('/revers/<word>')
+def revers(word):
+    if len(word) > 0:
+        return f"{word[::-1]}"
+    else:
+        return "Uncorrect data."
+
+@app.route('/user/<name>/<age>')
+def user_info(name, age):
+    if len(name) > 0 & age.isdigit(): 
+        if int(age) > 0:
+            return f"Hello, {name}. You are {age} years old."
+        else:
+            return "Uncorrect data."
+    else:
+        return "Uncorrect data."
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
