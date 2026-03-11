3e70a7c823c2da9050a0449265aa1c468e349c89
M-sim0993 <m-sim0993@mail.ru>
2026-03-11 16:04:26 +0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,9 +1,22 @@
 from app import app
+from flask import render_template
 
 # Определяем маршрут и привязываем его к функции
-@app.route('/')
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contact.html")
+
+""" @app.route('/')
 def hello():
-    return "Hello, Flask!"
+    return "Hello, Flask!" """
 
 @app.route('/hello')
 def hello_word():
