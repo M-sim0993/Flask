cc31cf8f1abb89e75869a4f7bab48e1174f90a41
M-sim0993 <m-sim0993@mail.ru>
2026-03-18 02:05:13 +0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,10 +1,24 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 # Определяем маршрут и привязываем его к функции
 @app.route("/")
-def home():
-    return render_template("index.html")
+def form():
+    return render_template("form.html")
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        name = request.form.get("name")  # Получаем имя из формы
+        email = request.form.get("email")  # Получаем email из формы
+        color = request.form.get("color")
+        profession = request.form.get("profession")
+        hobbies = request.form.getlist("hobbies")  # Для чекбоксов
+        level = request.form.get("level")
+        return render_template("result.html", name=name, email=email, color=color, profession=profession,
+                               hobbies=hobbies, level=level)
+    else:
+        return redirect(url_for("form"))  # Если запрос GET, возвращаем на форму
 
 @app.route("/about")
 def about():
